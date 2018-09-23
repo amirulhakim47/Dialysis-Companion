195a1121950185754049b23d54943a203adae7b6
yoi <amirulhakim47@gmail.com>
2018-09-23 16:25:19 +0800

    version 0.2

--- app/src/main/java/com/example/yoi/mainmenu/calimage.java
+++ app/src/main/java/com/example/yoi/mainmenu/calimage.java
@@ -1,39 +1,211 @@
 package com.example.yoi.mainmenu;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.text.InputType;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class calimage extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+import com.github.lzyzsd.circleprogress.CircleProgress;
+
+public class calimage extends AppCompatActivity {
 
 
-    String[] countryNames={"Small bottle","Large bottle","Canned","Take-away"};
-    int flags[] = {R.drawable.small, R.drawable.large, R.drawable.canned, R.drawable.bungkus};
+    private static final String TAG = "calimage";
+
+
+    final Context context = this;
+    private ViewPager mViewPager;
+
+    ProgressBar mProgress;
+    TextView tv4, tv13, tv14;
+    ImageView im1,im2,im3, im4, im5;
+    LinearLayout lin;
+    CircleProgress circularProgress;
+
+    private int progress = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calimage);
-        //Getting the instance of Spinner and applying OnItemSelectedListener on it
-        Spinner spin = (Spinner) findViewById(R.id.simpleSpinner);
-        spin.setOnItemSelectedListener(this);
 
-        CustomAdapter customAdapter=new CustomAdapter(getApplicationContext(),flags,countryNames);
-        spin.setAdapter(customAdapter);
+
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        tv4 = (TextView) findViewById(R.id.textView5);
+        circularProgress =(CircleProgress)findViewById(R.id.circle_progress);
+        circularProgress.setProgress(0);
+        circularProgress.setMax(100);
+
+        im1 = (ImageView) findViewById(R.id.cup);
+        im2 = (ImageView) findViewById(R.id.can);
+        im3 = (ImageView) findViewById(R.id.plus);
+        im4 = (ImageView) findViewById(R.id.bottle);
+        im5 = (ImageView) findViewById(R.id.jug);
+        lin = (LinearLayout) findViewById(R.id.anything);
+        lin.removeAllViews();
+        /*mProgress = (ProgressBar) findViewById(R.id.progressBar2);
+        mProgress.setProgress(0);
+        mProgress.setMax(1200);*/
+
+
+
+
+        if (circularProgress.getProgress() == circularProgress.getMax())
+        {
+
+            circularProgress.setProgress(0);
+            lin.removeAllViews();
+            Toast.makeText(calimage.this, "Warning! you have exceeded the water level limit", Toast.LENGTH_SHORT).show();
+
+        }
+
+
+       /* BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavView_Bar);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
+        Menu menu = bottomNavigationView.getMenu();
+        MenuItem menuItem = menu.getItem(0);
+        menuItem.setChecked(true);*/
+
+        im1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+               // mProgress.setProgress(mProgress.getProgress()+100);
+                circularProgress.setProgress(circularProgress.getProgress()+8);
+                TextView txtName = new TextView(calimage.this);
+                txtName.setId(View.generateViewId());
+                txtName.setText("Tea cup - 100 ml");
+
+                lin.addView(txtName);
+
+
+            }
+
+        });
+
+
+        im2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+              //  mProgress.setProgress(mProgress.getProgress()+250);
+                circularProgress.setProgress(circularProgress.getProgress()+30);
+                TextView txtName = new TextView(calimage.this);
+                txtName.setId(View.generateViewId());
+                txtName.setText("Soft drinks - 355 ml");
+
+                lin.addView(txtName);
+
+            }
+        });
+
+        im3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                LayoutInflater li = LayoutInflater.from(context);
+                View promptsView = li.inflate(R.layout.custom_dialog_calimage, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        context);
+
+                alertDialogBuilder.setView(promptsView);
+
+                final EditText userInput = (EditText) promptsView
+                        .findViewById(R.id.editTextDialogUserInput);
+                userInput.setInputType(InputType.TYPE_CLASS_NUMBER);
+                userInput.setRawInputType(Configuration.KEYBOARD_12KEY);
+
+                // set dialog message
+                alertDialogBuilder
+                        .setCancelable(false)
+                        .setPositiveButton("OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        int spin3 = Integer.parseInt(userInput.getText().toString()) ;
+                                        int spin2 = 1200;
+                                        int total = (spin3 * 100 + (spin2 >> 1)) / spin2;
+
+                                        circularProgress.setProgress(circularProgress.getProgress()+ total);
+                                        TextView txtName = new TextView(calimage.this);
+                                        txtName.setId(View.generateViewId());
+                                        txtName.setText("Custom - " + userInput.getText() + " ml");
+                                        lin.addView(txtName);
+                                    }
+                                })
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog,int id) {
+                                        dialog.cancel();
+                                    }
+                                });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+
+            }
+        });
+
+        im4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             //   mProgress.setProgress(mProgress.getProgress()+500);
+                circularProgress.setProgress(circularProgress.getProgress()+42);
+                TextView txtName = new TextView(calimage.this);
+                txtName.setId(View.generateViewId());
+                txtName.setText("Small bottle - 500 ml");
+
+                lin.addView(txtName);
+
+
+            }
+        });
+
+        im5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             //   mProgress.setProgress(mProgress.getProgress()+800);
+                circularProgress.setProgress(circularProgress.getProgress()+67);
+                TextView txtName = new TextView(calimage.this);
+                txtName.setId(View.generateViewId());
+                txtName.setText("Jug - 800 ml");
+
+                lin.addView(txtName);
+
+
+            }
+        });
+
     }
 
 
-    //Performing action onItemSelected and onNothing selected
-    @Override
-    public void onItemSelected(AdapterView<?> arg0, View arg1, int position,long id) {
-        Toast.makeText(getApplicationContext(), countryNames[position], Toast.LENGTH_LONG).show();
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> arg0) {
-        // TODO Auto-generated method stub
-    }
 }
